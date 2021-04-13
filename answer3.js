803ffefb4574a8731260bb44857967c7ffe446c1
MJ Phillip <32121892+MJPhillip@users.noreply.github.com>
2021-04-13 11:10:11 +0100

    Update answer3.js

--- answer3.js
+++ answer3.js
@@ -2,8 +2,7 @@ async function getFact() {
     try {
         const response = await fetch("https://cat-fact.herokuapp.com/facts");
         const results = await response.json();
-        const facts = results.all;
-        console.log(facts[8].text);
+        console.log(results[8].text);
     } catch (error) {
         console.log(error);
     } finally {
